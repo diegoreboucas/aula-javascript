a8d91cf2a0676247b78f0ac666d882e54fae5353
Diego Rebouças <diego.s.reboucas@gmail.com>
2021-06-22 21:58:43 -0300

    Commit arquivos js

---
+++ js/main.js
@@ -0,0 +1,67 @@
+/*
+const nome = "Diego Rebouças";
+const anoNascimento = 1987;
+const anoAtual = 2021;
+const time = "Ceará Sporting Club";
+let idade = anoAtual - anoNascimento;
+let frase = `${nome}, tem ${idade} anos e torce para ${time}.`;
+alert(frase);
+*/
+
+//let frutas = ["maça", "pêra", "laranja"];
+//frutas.push("uva");
+//frutas.pop();
+
+//console.log(frutas);
+//console.log(frutas.toString());
+//console.log(frutas.join(" | "));
+
+/*
+let fruta = { nome: "maça", cor: "vermelha" };
+console.log(fruta.nome);
+*/
+
+/*
+let listaFrutas = [
+    { nome: "maça", cor: "vermelha" },
+    { nome: "uva", cor: "roxo" },
+    { nome: "laranja", cor: "amarelo" }
+];
+console.log(listaFrutas[0].nome);
+*/
+
+/*
+const idade = prompt("Qual a sua idade?");
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+}
+*/
+
+/*
+let count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+let i;
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+*/
+
+/*
+let d = new Date();
+console.log("Hoje é " + d.getDate() + "/" + (d.getMonth() + 1));
+*/
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+console.log(soma(5, 10));
+
